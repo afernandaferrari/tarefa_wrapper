3391749362100b60cdf6b2ef0586a285139d1d7d
Fernanda Ferrari <fernandaferrari72@gmail.com>
2022-12-24 13:02:28 -0300

    first commit

---
+++ modulo9.java
@@ -0,0 +1,14 @@
+public class modulo9 {
+
+    public static void main (String args[]) {
+
+        int num1 = 29;
+
+        System.out.println(num1);
+
+        Integer num2 = num1;
+
+        System.out.println(num2);
+    }
+
+}
